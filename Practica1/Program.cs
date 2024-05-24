cff8f973c925f854f1cecfb277bd8ed3ad978246
Darli <Darli@DESKTOP-91VHOE3>
2024-05-23 22:16:40 -0400

    Agregar archivos de proyecto.

---
+++ Practica1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Ingrese su nombre");
+
+string Nombre =  Console.ReadLine();
+
+Console.WriteLine("Ingrese su Edad");
+
+int Edad = int.Parse(Console.ReadLine());
+
+if (Edad >= 18)
+{
+    Console.WriteLine("Eres Mayor de Edad");
+}
+else
+{
+    Console.WriteLine("Eres Menor de Edad");
+}
+
+
+Console.WriteLine("Imprimir Numero del 1 al 10 ");
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
